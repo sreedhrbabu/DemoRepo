333080a018d598664630031a933bca816c78fe1d
sreedhar <sreedharbabu.v@gmail.com>
2021-03-01 09:44:01 +0000

    Change Demo1 and two new files

--- Demo1.java
+++ Demo1.java
@@ -1,7 +1,8 @@
 class Demo1
 {
-	pulic static void main(String args[])
+	public static void main(String args[])
 	{
-		System.out,println("Java Demo Class1");
+		System.out.println("Java Demo Class1");
+		System.out.println(" This is second line in Demo class 1");
 	}
 }
